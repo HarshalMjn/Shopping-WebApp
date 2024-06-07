452513c73ee0a7042cbac97d0b89db4d47aa7cff
HarshalMjn <mahajanh006@gmail.com>
2024-06-07 17:21:03 +0530

    #

--- server/index.js
+++ server/index.js
@@ -3,29 +3,26 @@ const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config();  // Make sure this is at the top
 
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
-    origin: 'https://your-vercel-deployment-url.vercel.app',
+    origin: ["http://localhost:3000", "https://your-vercel-deployment-url.vercel.app"],  // Use an array to include both URLs
 }));
 
+// Connect to the database
 require("./config/database").connect();
 
+// Import and use routes
 const UserRoute = require("./routes/user");
 app.use("/api/v1/", UserRoute);
 
-//cart
 
-
-// Razorpay 
+// Razorpay payment routes
 const paymentRoutes = require("./routes/payment");
 app.use("/api/v1/", paymentRoutes);
-
-
 
 app.get("/", (req, res) => {
     res.send("<h1>Backend is Running and this is '/' Route</h1>");
