63b49af61931996a018b75a2098b69fc13bcef44
HarshalMjn <mahajanh006@gmail.com>
2024-06-13 11:46:31 +0530

    #

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // }));
 
 const corsOptions = {
-    origin:["https://shopping-web-rhefbegla-harshalmjns-projects.vercel.app","http://localhost:3000"],
+    origin:["https://shopping-web-app-fe.vercel.app","http://localhost:3000"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
   app.use(cors(corsOptions));
 
